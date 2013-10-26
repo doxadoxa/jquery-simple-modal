4000b5663dc6592bcbd0853801f9fd199aef2b77
Taras <doxa2006@ya.ru>
2013-10-26 22:06:08 +0400

    Add modal.jquery.js

---
+++ javascripts/modal.jquery.js
@@ -0,0 +1,59 @@
+/**********************************************
+Simle Jquery Modal
+https://github.com/doxadoxa/jquery-simple-modal
+MIT License
+***********************************************/
+(function($) {
+	var defaults = { 'bgColor' : '#000', 'opacity' : '0.8'};
+	var options;
+
+	$.fn.modal = function ( params ) {
+		options = $.extend({}, defaults, options, params);
+
+		var popupid = $(this).attr('rel');
+		var opacity = typeof( options.opacity ) != undefined ? options.opacity : '.80';
+
+
+		$(this).click( function () {
+			$('#' + popupid).fadeIn();
+
+			$('body').append('<div id="fade"></div>');
+			$("#fade").css( { 
+				'display' : 'none', /* Скрыто по умолчанию */
+				'background' : options.bgColor,
+				'position': 'fixed',
+				'left' : '0',
+				'top' : '0',
+				'width' : '100%', 
+				'height' : '100%',
+				'opacity' : options.opacity,
+				'z-index' : '9999',
+				'filter' : 'alpha(opacity=' + ( options.opacity * 100 ) + ')'
+			}).fadeIn();
+
+			var popuptopmargin = ($('#' + popupid).height() + 10) / 2;
+			var popupleftmargin = ($('#' + popupid).width() + 10) / 2;
+
+			$('#' + popupid).css({
+			'margin-top' : -popuptopmargin,
+			'margin-left' : -popupleftmargin,
+			'top' : '50%',
+			'left' : '50%',
+			'position' : 'fixed'
+			});
+		});
+
+
+		var closer = function() {
+			$('#' + popupid+", #fade" ).fadeOut();
+		}
+
+		if ( typeof( options.close ) != undefined ) {
+			$( "#" + options.close ).click( closer );
+		}
+
+		$("#fade").click( closer );
+		return this;
+	};
+
+})(jQuery);
